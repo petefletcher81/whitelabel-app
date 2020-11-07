16a0ed9642b636b8e4864c29b75cb8db41f8d54a
petefletcher <petefletch81@gmail.com>
2020-11-07 18:35:49 +0000

    test: add integration tests and error handling [PF]

--- functions/controllers/user.js
+++ functions/controllers/user.js
@@ -26,19 +26,25 @@ exports.adminLogin = async (req, res) => {
   // https://medium.com/javascript-in-plain-english/async-await-javascript-5038668ec6eb
 
   // set firebase admin sdk
-  // npm install fiebase admin / install firebase
-  console.log("data", email, password);
-
+  // npm install firebase admin / install firebase
   try {
     const data = await firebase
       .auth()
       .signInWithEmailAndPassword(email, password);
     const userToken = await data.user.getIdToken();
     token = userToken;
-    return res.json(data);
+    return res.json({ token });
     // explain and add signin test
   } catch (error) {
-    console.log(error);
-    res.send(error);
+    switch (error.code) {
+      case "auth/user-not-found":
+        return res.status(404).json({ error: "User not found" });
+        break;
+      case "auth/wrong-password":
+        return res.status(401).json({ error: "Invalid password" });
+        break;
+
+      default:
+    }
   }
 };
--- functions/index.js
+++ functions/index.js
@@ -7,11 +7,15 @@ const { adminLogin } = require("./controllers/user");
 const app = express(); // creates express app
 
 app.use(cors({ origin: true }));
-app.use(bodyParser.json()); // parse json object bodies
-app.use(bodyParser.urlencoded()); // parse bodies passed from a url ie form
+app.use(bodyParser.json({ extended: true })); // parse json object bodies --  where the Content-Type header matches the type option -- unicode encoding
+app.use(bodyParser.urlencoded({ extended: true })); // parse bodies passed from a url ie form -- Content-Type header matches the type option -- utf-8 encoding
 
 app.post("/admin", adminLogin);
 
+app.use((req, res) => {
+  res.send({ message: "Path does not exist" });
+});
+
 // onRequest takes a https function
 // app will turn into multiple different routes
-exports.api = functions.region("europe-west2").https.onRequest(app);
+exports.app = functions.region("europe-west2").https.onRequest(app);
