0106446a0ef3724746943e0ee9aae3078c5f2327
petefletcher <petefletch81@gmail.com>
2021-05-02 07:04:50 +0100

    style: add responsive styling and token to local storage [PF]

--- client/src/components/signin/SignIn.js
+++ client/src/components/signin/SignIn.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { attemptSignIn } from "../../utils/apiCalls";
+import "./SignIn.scss";
 
 const SignIn = () => {
   const [signIn, setSignIn] = useState({ password: "", email: "" });
@@ -16,8 +17,8 @@ const SignIn = () => {
     try {
       const signInRes = await attemptSignIn(signIn);
       setSuccess(true);
+      localStorage.setItem("token", signInRes.token);
       window.location.href = "/";
-      console.log("dispatch here");
     } catch (error) {
       const data = error.response?.data;
       setSignInError(data);
@@ -25,45 +26,48 @@ const SignIn = () => {
   };
 
   return (
-    <>
-      <form data-testid="signin-form">
-        <div className="signin-form--controls form-control">
-          <label htmlFor="email" className="visuallyhidden" />
+    <div className="signin-form-wrapper flex w-full">
+      <div className="signin-form form relative">
+        <form data-testid="signin-form">
+          <h2>Log In</h2>
+          <div className="signin-form--controls form-control">
+            <label htmlFor="email" className="visuallyhidden" />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={onChangeFormDetails}
+              required
+            />
+          </div>
+          <div className="signin-form--controls form-control">
+            <label htmlFor="password" className="visuallyhidden" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={onChangeFormDetails}
+              required
+            />
+            {success && (
+              <div className="signin-form--success">Sign In Successful</div>
+            )}
+            {signInError && (
+              <div className="signin-form-error">
+                {`${signInError.error}, please try again`}
+              </div>
+            )}
+          </div>
+          <label htmlFor="submit" className="visuallyhidden" />
           <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            onChange={onChangeFormDetails}
-            required
+            type="submit"
+            value="Sign In"
+            className="btn btn-primary"
+            onClick={handleSubmit}
           />
-        </div>
-        <div className="signin-form--controls form-control">
-          <label htmlFor="password" className="visuallyhidden" />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={onChangeFormDetails}
-            required
-          />
-          {success && (
-            <div className="signin-form--success">Sign In Successful</div>
-          )}
-          {signInError && (
-            <div className="signin-form-error">
-              {`${signInError.error}, please try again`}
-            </div>
-          )}
-        </div>
-        <label htmlFor="submit" className="visuallyhidden" />
-        <input
-          type="submit"
-          value="Sign In"
-          className="btn btn-primary"
-          onClick={handleSubmit}
-        />
-      </form>
-    </>
+        </form>
+      </div>
+    </div>
   );
 };
 
