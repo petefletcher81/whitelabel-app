8e7b866f38dbf1c96179c4e192993b64a17263ba
petefletcher <petefletch81@gmail.com>
2021-06-09 16:10:55 +0100

    style: add styling to Dashboard [PF]

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,12 +5,14 @@ import {
   getEnquiries,
 } from "../../utils/apiCalls";
 import DashboardContentCard from "../../utils/DashboardContentCard";
+import "./Dashboard.scss";
 
 const Dashboard = ({ setToggleContentModal }) => {
   const [enquiries, setEnquiries] = useState(null);
   const [content, setContent] = useState(null);
   const [images, setImages] = useState(null);
   const [error, setError] = useState(null);
+  const mobile = window.innerWidth < 990;
 
   useEffect(() => {
     const getAllEquiries = async () => {
@@ -51,92 +53,216 @@ const Dashboard = ({ setToggleContentModal }) => {
     getImages();
   }, []);
 
+  console.log(enquiries);
+
   return (
-    <div className="dashboard" data-testid="dashboard-screen">
-      <div className="dashboard__header">
-        <h1>Dashboard</h1>
+    <div
+      className="dashboard border-primary flex flex-col grid p-2"
+      data-testid="dashboard-screen"
+    >
+      <div className="dashboard__header flex">
+        <h1 className="text-title-m text-primary">Dashboard</h1>
       </div>
+      {/* enquiries table */}
+      <div className="dashboard__enquiry-content-wrapper grid">
+        <div
+          className={`dashboard__enquiries-wrapper border-wrapper border-bottom-sm mb-2 w-full`}
+          data-testid="manage-enquiries"
+        >
+          <div className="dashboard__heading py-1 flex text-white bg-primary">
+            Manage Enquiries
+          </div>
+          {/* headings enquiries */}
+          <div className="dashboard__enquiries--headings grid border-bottom-sm p-1 text-center">
+            <div className="dashboard__enquiries--heading text-primary">
+              Name
+            </div>
+            {!mobile && (
+              <div className="dashboard__enquiries--heading text-primary">
+                Email
+              </div>
+            )}
+            <div className="dashboard__enquiries--heading text-primary">
+              Created At
+            </div>
+            <div className="dashboard__enquiries--heading text-primary">
+              Contacted
+            </div>
+            <div className="dashboard__enquiries--heading text-primary"></div>
+          </div>
+          <div className="dashboard__enquiry-wrapper hidden scroll-y">
+            {enquiries &&
+              !error &&
+              enquiries.map((enquiry, i) => {
+                return (
+                  <section
+                    className={`dashboard__enquiries grid ${
+                      i % 2 === 0 ? "shaded" : null
+                    }`}
+                    key={enquiry.name}
+                  >
+                    {/* data enquiries */}
+                    <div className="dashboard__enquiries--contents grid p-1">
+                      <div className="dashboard__enquiries--content text-center">
+                        {enquiry.name}
+                      </div>
+                      {!mobile && (
+                        <div className="dashboard__enquiries--content text-center">
+                          {enquiry.email}
+                        </div>
+                      )}
+                      <div className="dashboard__enquiries--content text-center">
+                        {enquiry.createdAt}
+                      </div>
+                      <div className="dashboard__enquiries--content text-center">
+                        {enquiry.contacted ? "yes" : "no"}
+                      </div>
+                      <div className="flex">
+                        <button
+                          className="btn p-1"
+                          onClick={() =>
+                            setToggleContentModal({
+                              content: enquiry,
+                              type: "enquiries-content",
+                            })
+                          }
+                        >
+                          Edit
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+                );
+              })}
+          </div>
+        </div>
+        {/* flex column or grid */}
+        <div
+          className="dashboard__contents-content-wrapper border-wrapper border-bottom-sm mb-2 w-full hidden"
+          data-testid="manage-content"
+        >
+          <div className="dashboard__heading py-1 flex text-white bg-primary">
+            Manage Content
+          </div>
+          {/* headings content */}
+          <div className="dashboard__content--headings grid border-bottom-sm p-1 text-center">
+            <div className="dashboard__content--heading text-primary">
+              Heading
+            </div>
+            <div className="dashboard__content--heading text-primary">
+              Content
+            </div>
+            <div className="dashboard__content--heading-page text-primary">
+              Page
+            </div>
+            {!mobile && (
+              <div className="dashboard__content--heading text-primary">
+                Created At
+              </div>
+            )}
+            <div className="dashboard__content--heading text-primary"></div>
+          </div>
+
+          <div className="dashboard__content-wrapper hidden scroll-y">
+            {content &&
+              content?.map((section, index) => {
+                return (
+                  <div
+                    className={`dashboard__content grid ${
+                      index % 2 === 0 ? "shaded" : null
+                    }`}
+                    key={`${section.createdAt}-${index}-dashboard`}
+                  >
+                    <DashboardContentCard
+                      mobile={mobile}
+                      page={section.page}
+                      section={section}
+                      index={index}
+                      datatestid={"home-content-dashboard"}
+                      setToggleContentModal={setToggleContentModal}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </div>
+
       <div
-        className="dashboard__enquiries-wrapper"
-        data-testid="manage-enquiries"
+        className="dashboard__images-wrapper border-wrapper border-bottom-sm mb-2 w-full hidden"
+        data-testid="manage-images"
       >
-        <h2>Manage Enquiries</h2>
-        {enquiries &&
-          !error &&
-          enquiries.map((enquiry) => {
+        {/* headings images */}
+        <div className="dashboard__heading py-1 flex text-white bg-primary">
+          Manage Images
+        </div>
+        <div className="dashboard__images--headings grid border-bottom-sm p-1 text-center">
+          {!mobile && (
+            <>
+              <div className="dashboard__images--heading text-primary">
+                Name
+              </div>
+              <div className="dashboard__images--heading text-primary">
+                Created At
+              </div>
+            </>
+          )}
+          <div className="dashboard__images--heading text-primary">Image</div>
+          <div className="dashboard__images--heading-page text-primary">
+            section
+          </div>
+          <div className="dashboard__images--heading text-primary"></div>
+        </div>
+
+        {/* images content */}
+        {images &&
+          images.map((image, index) => {
             return (
               <section
-                className="dashboard__enquiries--enquiry"
-                key={enquiry.name}
+                className={`dashboard__images--content ${
+                  index % 2 === 0 ? "shaded" : null
+                }`}
+                key={image.id}
               >
-                <div className="dashboard__enquiries--enquiry-name">
-                  {enquiry.name}
+                <div className="dashboard__images--contents grid p-1">
+                  {!mobile && (
+                    <>
+                      <div className="dashboard__image-name">{image.key}</div>
+                      <div className="dashboard__images-created-at">
+                        {image.createdAt}
+                      </div>
+                    </>
+                  )}
+                  {image.image && (
+                    <div className="dashboard__image-image">
+                      <img src={image.image} className="w-full h-full" />
+                    </div>
+                  )}
+                  {image.banner && (
+                    <div className="dashboard__image-banner">
+                      <img src={image.banner} className="w-full h-full" />
+                    </div>
+                  )}
+                  {image.gallery && (
+                    <div className="dashboard__image-gallery">
+                      <img src={image.gallery} className="w-full h-full" />
+                    </div>
+                  )}
+                  <div className="dashboard__image-section text-center">
+                    {image.section}
+                  </div>
+                  <button
+                    className="btn p-1"
+                    onClick={() =>
+                      setToggleContentModal({
+                        content: image,
+                        type: "image-content",
+                      })
+                    }
+                  >
+                    Edit
+                  </button>
                 </div>
-                <div className="dashboard__enquiries--enquiry-email">
-                  {enquiry.email}
-                </div>
-                <div className="dashboard__enquiries--enquiry-created-at">
-                  {enquiry.createdAt}
-                </div>
-                <button
-                  onClick={() =>
-                    setToggleContentModal({
-                      content: enquiry,
-                      type: "enquiries-content",
-                    })
-                  }
-                >
-                  Edit
-                </button>
-              </section>
-            );
-          })}
-      </div>
-      {/* flex column or grid */}
-      <div className="dashboard__content-wrapper" data-testid="manage-content">
-        <h2>Manage Content</h2>
-        {content &&
-          content?.map((section, index) => {
-            return (
-              <div key={`${section.createdAt}-${index}-dashboard`}>
-                <DashboardContentCard
-                  page={section.page}
-                  section={section}
-                  index={index}
-                  datatestid={"home-content-dashboard"}
-                  setToggleContentModal={setToggleContentModal}
-                />
-              </div>
-            );
-          })}
-      </div>
-      <div className="dashboard__images-wrapper" data-testid="manage-images">
-        <h2>Manage Images</h2>
-        {images &&
-          images.map((image) => {
-            return (
-              <section className="dashboard__images" key={image.id}>
-                <div className="dashboard__image-id">{image.id}</div>
-                {image.image && (
-                  <div className="dashboard__image-image">{image.image}</div>
-                )}
-                {image.banner && (
-                  <div className="dashboard__image-banner">{image.banner}</div>
-                )}
-                <div className="dashboard__image-section">{image.section}</div>
-                <div className="dashboard__content-created-at">
-                  {image.createdAt}
-                </div>
-                <button
-                  onClick={() =>
-                    setToggleContentModal({
-                      content: image,
-                      type: "image-content",
-                    })
-                  }
-                >
-                  Edit
-                </button>
               </section>
             );
           })}
--- client/src/utils/DashboardContentCard.js
+++ client/src/utils/DashboardContentCard.js
@@ -5,28 +5,43 @@ const DashboardContentCard = ({
   index,
   datatestid,
   setToggleContentModal,
+  mobile,
 }) => {
   return (
     <>
       <section
-        className="dashboard__content"
+        className="dashboard__content--contents grid p-1"
         key={`${section.createdAt}-${index}-dashboard`}
         data-testid={datatestid}
       >
-        <div className="dashboard__content-heading">{section.heading}</div>
-        <div className="dashboard__content-content">{section.content}</div>
-        <div className="dashboard__content-created-at">{section.createdAt}</div>
-        <button
-          onClick={() =>
-            setToggleContentModal({
-              page: section.page,
-              item: section,
-              type: "site-content",
-            })
-          }
-        >
-          Edit
-        </button>
+        <div className="dashboard__content-heading text-center">
+          {section.heading}
+        </div>
+        <div className="dashboard__content-content h-4 hidden scroll-y">
+          {section.content}
+        </div>
+        <div className="dashboard__content-page text-center">
+          {section.page}
+        </div>
+        {!mobile && (
+          <div className="dashboard__content-created-at text-center">
+            {section.createdAt}
+          </div>
+        )}
+        <div className="flex">
+          <button
+            className="btn p-1    "
+            onClick={() =>
+              setToggleContentModal({
+                page: section.page,
+                item: section,
+                type: "site-content",
+              })
+            }
+          >
+            Edit
+          </button>
+        </div>
       </section>
     </>
   );
