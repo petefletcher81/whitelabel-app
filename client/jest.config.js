556d49105923b7ba6a6a7f6d386d0af8b191a239
petefletcher <petefletch81@gmail.com>
2021-07-04 06:51:40 +0100

    hosting: set up hosting and actions [pf]

--- client/jest.config.js
+++ client/jest.config.js
@@ -61,9 +61,7 @@ module.exports = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: ["node_modules", "src"],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
@@ -115,9 +113,7 @@ module.exports = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: ["<rootDir>/src"],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
